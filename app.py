72e0d8e24172c9cf0aad3ce2ac0e9b929002cd08
Vinicius Lucchesi <viniciuslucchesi.ti@gmail.com>
2023-09-13 19:54:01 -0300

    Adding template to our route

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
+from database import conn
 from magick import Magick
 
 app = Magick()
 
 
-@app.route('^/$')
-def hello_world():
-    return "<h1>Funciona!</h1>"
+def get_posts_from_database(post_id: int = None) -> list[dict[str, str]]:
+    cursor = conn.cursor()
+    fields = ('id', 'title', 'content', 'author')
+    if post_id:
+        results = cursor.execute('SELECT * FROM post WHERE id = ?;', post_id)
+    else:
+        results = cursor.execute('SELECT * FROM post;')
+    return [dict(zip(fields, post)) for post in results]
 
 
-# @app.route('^/(?P<id>\d{1,})$')
-# def somente_digito():
+
+@app.route('^/$', template="list.template.html")
+def index():
+    posts = get_posts_from_database()
+    return { 'post_list': posts }
+
+# @app.route('^/(?P<id>\d{1,})$', template="post.template.html")
+# def somente_digito(id: str):
 #     return "<h1>A URL desta página só contém digitos!</h1>"
 
 
